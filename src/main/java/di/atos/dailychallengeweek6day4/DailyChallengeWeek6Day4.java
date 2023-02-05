e2d2f67c729f957b43b9e130c022da81eda4d7c4
seydou soro <w.soro@sectester.io>
2023-02-05 10:06:32 +0000

    DailyChallenge

---
+++ src/main/java/di/atos/dailychallengeweek6day4/DailyChallengeWeek6Day4.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package di.atos.dailychallengeweek6day4;
+
+import di.atos.dailychallengeweek6day4.challenge.Calculator;
+
+
+/**
+ *
+ * @author SORO
+ *@date 29/01/2023
+ */
+public class DailyChallengeWeek6Day4 {
+
+    public static void main(String[] args) {
+        Calculator obj = new Calculator(10, 94);
+
+        System.out.println(obj.add());
+        System.out.println(obj.subtract());
+        System.out.println(obj.multiply());
+        System.out.println(obj.divide());
+    }
+}
+
+
+/**
+ *
+ * @author SORO
+ *@date 29/01/2023
+ */
---
+++ src/main/java/di/atos/dailychallengeweek6day4/challenge/Calculator.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package di.atos.dailychallengeweek6day4.challenge;
+
+
+/**
+ *
+ * @author SORO
+ *@date 29/01/2023
+ */
+public class Calculator {
+    // write class fields here
+    private double num1;
+    private double num2;
+
+    public Calculator(double num1, double num2) {
+        //write definition here
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double add() {
+        //write definition here
+        return this.num1 + this.num2;
+    }
+
+    public double subtract() {
+        //write definition here
+        return this.num1 - this.num2;
+    }
+
+    public double multiply() {
+        //write definition here
+        return this.num1 * this.num2;
+    }
+
+    public double divide() {
+        //write definition here
+        return this.num1 / this.num2;
+    }
+
+}
+
+
+
+/**
+ *
+ * @author SORO
+ *@date 29/01/2023
+ */
